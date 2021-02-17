dda827bd5775d6fc0787268b84b110d737b70836
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-02-17 16:47:29 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,99 @@
+function tell() {
+  var q=document.getElementById("question").value;
+  if (q=="hello"){
+    alert("hi "+user);
+  } else if (q=="Hello"){
+    alert("Hi "+user);
+  } else if (q=="hi"){
+    alert("Hello"+user);
+  } else if (q=="Hi"){
+    alert("Hello "+user);
+  } else if (q=="tell me a joke"){
+    alert("what will you call a rose which wants to go to moon Gulab Ja moon");
+  } else if (q=="Tell me a joke"){
+    alert("What you will call a rose which wants to go to moon Gulab Ja Moon");
+  } else if (q=="One more joke please"){
+    alert("what do elves learn at school elf-a-bet");
+  } else if (q=="one more joke please"){
+    alert("what do elves larn at school elf-a-bet");
+  } else if (q=="How are you"){
+    alert("I am fine till you play me");
+  } else if (q=="how are you"){
+    alert("I am fine till you play me");
+  } else if (q=="what do do"){
+    alert("I connect you to your friends and entertain you");
+  } else if (q=="What do you do"){
+    alert("I connect you to your friends and entertain you");
+  } else if (q=="what do you eat"){
+    alert("I eat converstions emojis and code");
+  } else if (q=="What do you eat"){
+    alert("I eat conversations emojis and code");
+  } else if (q=="Where do you live "){
+    alert("I live in BPCCC internet");
+  } else if (q=="where do you live"){
+    alert("I live in BPCCC internet");
+  }else if (q=="How are you?"){
+    alert("I am fine till you play me");
+  } else if (q=="how are you?"){
+    alert("I am fine till you play me");
+  } else if (q=="what do do?"){
+    alert("I connect you to your friends and entertain you");
+  } else if (q=="What do you do?"){
+    alert("I connect you to your friends and entertain you");
+  } else if (q=="what do you eat?"){
+    alert("I eat converstions emojis and code");
+  } else if (q=="What do you eat?"){
+    alert("I eat conversations emojis and code");
+  } else if (q=="Where do you live?"){
+    alert("I live in BPCCC internet");
+  } else if (q=="where do you live?"){
+    alert("I live in BPCCC internet");
+  } else if (q=="who made you"){
+    alert("Anubhav Singh made me");
+  } else if (q=="Who made you"){
+    alert("Anubhav Singh made me");
+  } else if (q=="Who made you?"){
+    alert("AnuBhav Singh made me");
+  } else if (q=="who made you?"){
+    alert("Anubhav Singh Made me");
+  } else if (q=="Who are the members of BPCCC"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  } else if (q=="who are the members of BPCCC"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  } else if (q=="Who are the members of bpccc"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  } else if (q=="who are the members of bpccc"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  }else if (q=="Who are the members of BPCCC?"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  } else if (q=="Who are the members of bpccc?"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  }else if (q=="who are the members of BPCCC?"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  } else if (q=="who are the members of bpccc?"){
+    alert("The members of BPCCC are Anubhav,Soumajit,Debhargo,Satyam,Suryakanti,ADB");
+  } else if (q=="When you were born"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if (q=="When you were born?"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if (q=="when you were born?"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if (q=="when you were born"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if (q=="When were you born"){
+    alert("You all created me on 2016 by playing BPCCC");
+  }else if (q=="When were you born?"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if (q=="when were you born"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if (q=="when were you born?"){
+    alert("You all created me on 2016 by playing BPCCC");
+  } else if(q=="What is you name"){
+            alert("My name is Bacha Party Chuta Chuti Club but you can Call me BPCCC");
+            }else if(q==""){
+          alert("sorry what did you said please type it");
+          }else {
+    alert("sorry give feedback to anubhav to add this");
+    window.location.replace("https://docs.google.com/forms/d/e/1FAIpQLSeYvIglhTmQAMKdpTw-CUapX5bpkFfwwilbvZl5fSBM03aJ0Q/viewform?usp=sf_link");
+  }
+}
