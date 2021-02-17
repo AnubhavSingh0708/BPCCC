55a89dbc5fbd4e51843c53654ed5609596880b0a
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-02-17 16:57:59 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,19 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('fox-store').then((cache) => cache.addAll([
+      '/BPCCC/',
+      '/BPCCC/index.html',
+      '/BPCCC/index.js',
+       '/BPCCC/sketch.js',
+        '/BPCCC/script.js',
+      '/BPCCC/style.css',
+    ])),
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  console.log(e.request.url);
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request)),
+  );
+});
