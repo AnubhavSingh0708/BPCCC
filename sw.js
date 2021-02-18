2cf42e016b4162994a23fb880573a1f44d2f2dae
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-02-18 17:11:03 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,9 +2,7 @@ self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open('fox-store').then((cache) => cache.addAll([
       '/BPCCC/',
-      '/BPCCC/index.html',
       '/BPCCC/index.js',
-       '/BPCCC/sketch.js',
         '/BPCCC/script.js',
       '/BPCCC/style.css',
       'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.js',
@@ -13,7 +11,7 @@ self.addEventListener('install', (e) => {
   );
 });
 
-self.addEventListener('fetch', (e) => {
+self.addEventListener('fetch', (e) => 
   console.log(e.request.url);
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request)),
