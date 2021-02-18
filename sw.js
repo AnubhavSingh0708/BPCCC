0146a79c81db51d0a7c4f30ad9cd14bfaf81355b
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2021-02-18 22:54:06 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,34 @@
 self.addEventListener('install', (e) => {
+
   e.waitUntil(
+
     caches.open('fox-store').then((cache) => cache.addAll([
+
       '/BPCCC/',
+
       '/BPCCC/index.html',
+
       '/BPCCC/index.js',
-'/BPCCC/sketch.js',
-        '/BPCCC/script.js',
+
       '/BPCCC/style.css',
+
+'/BPCCC/script.js',
+      '/BPCCC/sketch.js',
+
     ])),
+
   );
+
 });
 
-self.addEventListener('fetch', (e) => 
+self.addEventListener('fetch', (e) => {
+
   console.log(e.request.url);
+
   e.respondWith(
+
     caches.match(e.request).then((response) => response || fetch(e.request)),
+
   );
+
 });
